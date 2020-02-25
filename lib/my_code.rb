20bdb1bb23944386b3d14437dc9028210c994a99
Bryant Omoregie <unclebryant@gmail.com>
2020-02-25 22:59:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,1 @@
-# set up an innumerable and you pass your element to a block or you yield it to a block. What it means to yield is to pass elemtn thru a block and do the work with it. 
+# The yield keyword executes the block passed into the method.
